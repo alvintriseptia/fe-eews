a40faadc635f389158de543b2d407b6934915761
alvintriseptia <alvintryseptia@gmail.com>
2023-11-21 08:10:00 +0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className='flex justify-center items-center h-screen text-7xl font-bold'>
+        HELLO WORLD
+      </div>
+    );
+  }
+}
