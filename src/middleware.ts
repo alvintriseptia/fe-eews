68891f353b3e565d6037146fa2f833434b7343d4
alvintriseptia <alvintryseptia@gmail.com>
2023-12-06 09:55:26 +0800

    uncomment middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,33 @@
-import { getCookie, getCookies, setCookie } from "cookies-next";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-	// const session = request.cookies.get("session");
+	const session = request.cookies.get("session");
 
-	// //Return to /login if don't have a session
-	// if (!session) {
-	// 	return NextResponse.redirect(new URL("/login", request.url));
-	// }
+	//Return to /login if don't have a session
+	if (!session) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
 
-	// //Call the authentication endpoint
-	// const responseAPI = await fetch(`${request.nextUrl.origin}/api/auth/session`, {
-	// 	headers: {
-	// 		Cookie: `session=${session}`,
-	// 	},
-	// });
+	//Call the authentication endpoint
+	const responseAPI = await fetch(
+		`${request.nextUrl.origin}/api/auth/session`,
+		{
+			headers: {
+				Cookie: `session=${session}`,
+			},
+		}
+	);
 
-	// //Return to /login if token is not authorized
-	// if (responseAPI.status !== 200) {
-	// 	return NextResponse.redirect(new URL("/login", request.url));
-	// }
+	//Return to /login if token is not authorized
+	if (responseAPI.status !== 200) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
 
-  // // if path /login and have session, redirect to /
-  // if(request.nextUrl.pathname === "/login") {
-  //   return NextResponse.redirect(new URL("/", request.url));
-  // }
+	// if path /login and have session, redirect to /
+	if (request.nextUrl.pathname === "/login") {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
 
 	return NextResponse.next();
 }
